3034a4981643835616bea3250d7ffb47f608a366
ThiagoJO0705 <rm551624@fiap.com.br>
2025-06-23 23:09:23 -0300

    adicionando dashboard

--- data_broker/index.js
+++ data_broker/index.js
@@ -1,10 +1,29 @@
-const io = require('socket.io')(3000);  // abre WS em 3000
-console.log("Broker conectado, aguardando conexao...")
-io.on('connection', socket => {
-  console.log('Cliente conectado:', socket.id);
-  socket.on('attention', data => {
-    console.log("\n=-=-=-=-received data=-=-=-=-")
-    console.log(data)
-    socket.broadcast.emit('attention', data);
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado ao broker:', socket.id);
+
+  socket.on('attention', (data) => {
+    console.log("\n--- Attention recebido do acquisition.py ---");
+    console.log(data);
+
+    // Reenvia o dado para todos os dashboards conectados
+    io.emit('attention', data);
   });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Broker + Dashboard rodando na porta 3000');
 });
